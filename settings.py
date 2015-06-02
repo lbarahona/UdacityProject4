8fe07751086bb323e49ff33c602ca16c81b1f2d8
Lester Miller Barahona <lbarahona@flukyfactory.com>
2015-06-02 10:34:10 -0600

    Project Working

---
+++ settings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = '1046229632118-5v1pnbtupvmlpn2aekb3nac23beqfapl.apps.googleusercontent.com'
+ANDROID_CLIENT_ID = '109241301412-b4e8k2rkt7uv1f8rgd440178ie274h2c.apps.googleusercontent.com'
+IOS_CLIENT_ID = '109241301412-qkgtbkavjfaloq4jsus2nhn0j160usn0.apps.googleusercontent.com'
+ANDROID_AUDIENCE = WEB_CLIENT_ID
